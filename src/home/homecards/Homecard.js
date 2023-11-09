1e8384ed41143154938636db391f3af7763afb8b
anjali-1607 <anjalirajpoot1609@gmail.com>
2023-11-09 21:33:39 +0530

    carousel

---
+++ src/home/homecards/Homecard.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Homecard.css";
+
+export default function Homecard() {
+  return (
+    <>
+      <div className="a_main2"></div>
+    </>
+  );
+}
---
+++ src/navbar/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Navbar.css";
+
+export default function Navbar() {
+  return (
+    <>
+      <header>
+        <div className="a_header_top"></div>
+        <div className="a_header_bottom"></div>
+      </header>
+    </>
+  );
+}
