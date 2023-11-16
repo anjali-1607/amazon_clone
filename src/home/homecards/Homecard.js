5bcdee87ee1169275cbbb5aedf75789618581877
anjali-1607 <anjalirajpoot1609@gmail.com>
2023-11-17 00:32:12 +0530

    section1 layout done

--- src/home/homecards/Homecard.js
+++ src/home/homecards/Homecard.js
@@ -1,11 +1,14 @@
 import React from "react";
 import "./Homecard.css";
+import Cards from "./section1/Cards";
 
 export default function Homecard() {
   return (
     <>
       <div className="a_main11">
-        <div className="a_main2 "></div>
+        <div className="a_main2 ">
+          <Cards />
+        </div>
       </div>
     </>
   );
---
+++ src/home/homecards/section1/Cards.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Cards.css";
+
+export default function Cards() {
+  return (
+    <>
+      <div className="section1_grand">
+        <div className="section1_cards">A</div>
+        <div className="section1_cards">B</div>
+        <div className="section1_cards">C</div>
+        <div className="section1_cards">D</div>
+      </div>
+    </>
+  );
+}
