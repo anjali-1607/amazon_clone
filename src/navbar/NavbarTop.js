71eeb5016ce38b3503c9a60f5c5fc8a2e5eaca74
anjali-1607 <anjalirajpoot1609@gmail.com>
2023-11-30 01:35:13 +0530

    navbar_buttons

--- src/navbar/NavbarTop.js
+++ src/navbar/NavbarTop.js
@@ -1,13 +1,26 @@
 import React from "react";
 import "./Navbar.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
 
 export default function NavbarTop() {
   return (
     <>
       <div className="a_header_top ">
-        <button className="top_nav_btn">
-          {/* <img src={require("./amazon_logo.png")} className="amazon_nav_logo" /> */}
+        <button className="top_nav_btn1">
+          <img src={require("./amazon_logo.png")} className="amazon_nav_logo" />
         </button>
+        <button className="top_nav_btn2">
+          <div className="nav_btn2_1">Deliver to</div>
+          <div className="nav_btn2_2">
+            {" "}
+            <i className="bi bi-geo-alt  location_icon_nav"></i>India
+          </div>
+        </button>
+        <div className="nav_search_all"></div>
+        <button className="top_nav_btn3"></button>
+        <button className="top_nav_btn4"></button>
+        <button className="top_nav_btn5"></button>
+        <button className="top_nav_btn6"></button>
       </div>
     </>
   );
